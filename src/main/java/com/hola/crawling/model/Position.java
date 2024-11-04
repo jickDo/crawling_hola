29e697d00bdebb552584e1b6069f109dd3615630
jickDo <pocard0117@naver.com>
2024-11-04 22:29:40 +0900

    chore: dto 구조 조정

--- src/main/java/com/hola/crawling/model/Position.java
+++ src/main/java/com/hola/crawling/model/Position.java
@@ -1,13 +1,9 @@
 package com.hola.crawling.model;
 
-import com.hola.crawling.entity.PostEntity;
-
 import lombok.Builder;
 
 @Builder
 public record Position(
 	Long id,
-	String position,
-	PostEntity postEntity
-) {
+	String position) {
 }
--- src/main/java/com/hola/crawling/model/Post.java
+++ src/main/java/com/hola/crawling/model/Post.java
@@ -1,22 +1,10 @@
 package com.hola.crawling.model;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import lombok.Builder;
 
+@Builder
 public record Post(
 	Long id,
-	String externalId,
-	List<Language> languages,
-	List<Position> positions,
-	boolean isClosed,
-	LocalDateTime startDate,
-	LocalDateTime endDate,
-	String type,
-	String onlineOrOffline,
-	String contactType,
-	String expectedPeriod,
-	String title,
-	LocalDateTime createdAt,
-	String state
+	String externalId
 ) {
 }
