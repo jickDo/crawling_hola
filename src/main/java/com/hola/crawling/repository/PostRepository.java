d6dc933005f104d9ee10201db1cb4fa96ad9442f
jickDo <pocard0117@naver.com>
2024-11-04 22:30:19 +0900

    chore: 중복안되는 id 반환

--- src/main/java/com/hola/crawling/repository/PostRepository.java
+++ src/main/java/com/hola/crawling/repository/PostRepository.java
@@ -1,8 +1,19 @@
 package com.hola.crawling.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.hola.crawling.entity.PostEntity;
 
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
+
+	@Query("""
+		SELECT p.externalId 
+		FROM PostEntity p 
+		WHERE p.externalId IN :ids
+		""")
+	List<String> findExistingExternalIds(@Param("ids") List<String> externalIds);
 }
