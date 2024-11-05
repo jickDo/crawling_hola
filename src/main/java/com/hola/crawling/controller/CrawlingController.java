1fc8abf5ce6f5ca5391d40a6806f0ccbf4164340
jickDo <pocard0117@naver.com>
2024-11-05 16:04:47 +0900

    feat: 외부 환경 노출을 때문에 api key 생성

--- src/main/java/com/hola/crawling/controller/CrawlingController.java
+++ src/main/java/com/hola/crawling/controller/CrawlingController.java
@@ -1,6 +1,10 @@
 package com.hola.crawling.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +21,20 @@ public class CrawlingController {
 
 	private final FacadeService facadeService;
 
+	@Value("${api.key}")
+	private String apiKey;
+
 	@GetMapping("/hola")
-	public void crawlHola() {
+	public ResponseEntity<?> crawlHola(
+		@RequestHeader(value = "X-API-KEY", required = true) String receivedApiKey) {
+
+		if (!apiKey.equals(receivedApiKey)) {
+			log.warn("잘못된 API 키 요청 감지");
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		}
+
 		log.info("크롤링 시작");
 		facadeService.crawlAndSave();
+		return ResponseEntity.ok().build();
 	}
 }
