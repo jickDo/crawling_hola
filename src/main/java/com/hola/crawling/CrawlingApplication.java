7a83b2c2945324494cfca0c454c1eda38e624950
jickDo <pocard0117@naver.com>
2024-11-05 16:50:06 +0900

    feat: ci cd환경에서 작동되게 cron job으로 변경

--- src/main/java/com/hola/crawling/CrawlingApplication.java
+++ src/main/java/com/hola/crawling/CrawlingApplication.java
@@ -1,12 +1,25 @@
 package com.hola.crawling;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.hola.crawling.service.FacadeService;
+
 @SpringBootApplication
-public class CrawlingApplication {
+public class CrawlingApplication implements CommandLineRunner {
+
+	@Autowired
+	private FacadeService facadeService;
 
 	public static void main(String[] args) {
-		SpringApplication.run(CrawlingApplication.class, args);
+		SpringApplication.run(CrawlingApplication.class, args)
+			.close();
+	}
+
+	@Override
+	public void run(String... args) {
+		facadeService.crawlAndSave();
 	}
 }
