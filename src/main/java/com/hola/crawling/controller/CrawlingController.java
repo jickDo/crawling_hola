296c2ec33943a41771a3fe2d5c25bf7e33339d9f
jickDo <pocard0117@naver.com>
2024-11-06 17:55:47 +0900

    chore: api단 임시 주석

--- src/main/java/com/hola/crawling/controller/CrawlingController.java
+++ src/main/java/com/hola/crawling/controller/CrawlingController.java
@@ -1,42 +1,42 @@
-package com.hola.crawling.controller;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.hola.crawling.service.FacadeService;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@RestController
-@RequestMapping("/api/crawling")
-@RequiredArgsConstructor
-public class CrawlingController {
-
-	private final FacadeService facadeService;
-
-	@Value("${api.key}")
-	private String apiKey;
-
-	@GetMapping("/hola")
-	public ResponseEntity<?> crawlHola(
-		@RequestHeader(value = "X-API-KEY", required = true) String receivedApiKey) {
-
-		if (!apiKey.equals(receivedApiKey)) {
-			log.warn("잘못된 API 키 요청 감지");
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-		}
-
-		log.info("크롤링 시작");
-		facadeService.crawlAndSave();
-		return ResponseEntity.ok().build();
-	}
-
-	//수동호출용 ci cd환경에서 쓸일없음
-}
+// package com.hola.crawling.controller;
+//
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestHeader;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+//
+// import com.hola.crawling.service.FacadeService;
+//
+// import lombok.RequiredArgsConstructor;
+// import lombok.extern.slf4j.Slf4j;
+//
+// @Slf4j
+// @RestController
+// @RequestMapping("/api/crawling")
+// @RequiredArgsConstructor
+// public class CrawlingController {
+//
+// 	private final FacadeService facadeService;
+//
+// 	@Value("${api.key}")
+// 	private String apiKey;
+//
+// 	@GetMapping("/hola")
+// 	public ResponseEntity<?> crawlHola(
+// 		@RequestHeader(value = "X-API-KEY", required = true) String receivedApiKey) {
+//
+// 		if (!apiKey.equals(receivedApiKey)) {
+// 			log.warn("잘못된 API 키 요청 감지");
+// 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+// 		}
+//
+// 		log.info("크롤링 시작");
+// 		facadeService.crawlAndSave();
+// 		return ResponseEntity.ok().build();
+// 	}
+//
+// 	//수동호출용 ci cd환경에서 쓸일없음
+// }
